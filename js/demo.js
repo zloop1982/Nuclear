4cbb568cc0bc8c873ec309183b63fcc10f429e35
张磊 <kmdjs@qq.com>
2015-06-04 15:20:33 +0800

    新加两个例子

--- js/demo.js
+++ js/demo.js
@@ -1,11 +1,11 @@
-﻿var HelloMessage = Nuclear.extend({
+﻿var HelloMessage = Nuclear.create({
     render: function () {
         return '<div>Hello , {{name}} !</div>';
     }
 })
 new HelloMessage("#helloContainer", { name: "Nuclear" });
 
-var Timer = Nuclear.extend({
+var Timer = Nuclear.create({
     install: function () {
         //react这里不需要加bind(this),会导致对javascript上下文的误解
         this.interval = setInterval(this.tick.bind(this), 1000);
@@ -23,7 +23,7 @@ var Timer = Nuclear.extend({
 
 new Timer("#timerContainer", { secondsElapsed: 0 });
 
-var TodoApp = Nuclear.extend({
+var TodoApp = Nuclear.create({
     onRefresh: function () {
         this.form = this.node.querySelector("form");
         this.textBox = this.node.querySelector("input");
@@ -46,7 +46,7 @@ var TodoApp = Nuclear.extend({
 
 new TodoApp("#todoContainer", { items: [] });
 
-var MarkdownEditor = Nuclear.extend({
+var MarkdownEditor = Nuclear.create({
     install: function () {
         this.option.html = marked(this.option.value);
     },
@@ -78,3 +78,115 @@ var MarkdownEditor = Nuclear.extend({
 
 new MarkdownEditor("#markdownContainer",
                    { value: 'Type some *markdown* here!', html: '' })
+
+
+var Progress = Nuclear.create({
+    install: function () {
+        this.option.percent = function () {
+            return this.value * 100 + "%";
+        }
+    },
+    render: function () {
+        if (this.option.displayNumber) {
+            return '<div class="progress">\
+                                    <div style="width:{{percent}};" class="progress-bar progress-bar-primary">{{percent}}</div>\
+                                </div>';
+        } else {
+            return '<div class="progress">\
+                                    <div style="width:{{percent}};" class="progress-bar progress-bar-primary"></div>\
+                                </div>';
+        }
+    }
+})
+var progress = new Progress("#progressContainer", { value: 0.6, displayNumber: true });
+//操作数据自动刷新Dom
+progress.option.value = 0.3;
+var progress = new Progress("#progressContainer2", { value: 0.6, displayNumber: false });
+
+
+var Button = Nuclear.create({
+    install: function () {
+        this.option.disable = false;
+    },
+    installed: function () {
+        this.node.addEventListener("click", function (evt) {
+            if (this.option.onClick) {
+                this.option.onClick.call(this.node, evt);
+            }
+        }.bind(this), false);
+    },
+    render: function () {
+        return ' <a class="btn {{#disable}}disable{{/disable}}">{{text}}</a>';
+    }
+})
+
+var SwitchButton = Button.create({
+    installed: function () {
+        this.node.addEventListener("click", function (evt) {
+            this.option.disable = !this.option.disable;
+            if (this.option.disable) {
+                this.option.close && this.option.close();
+            } else {
+                this.option.open && this.option.open();
+            }
+        }.bind(this), false);
+    }
+})
+
+var CountdownButton = Button.create({
+    install: function () {
+        //可通过访问父类方法
+        // this._super();              
+    },
+    installed: function () {
+        this.node.addEventListener("click", function (evt) {
+            if (!this.option.disable) {
+                this.option.textBak = this.option.text;
+                this.option.valueBak = this.option.value;
+                this.option.value--;
+                this.option.text = this.option.waitingText.replace("{}", this.option.value);
+                this.option.disable = !this.option.disable;
+                this.interval = setInterval(this.tick.bind(this), 1000);
+            }
+        }.bind(this), false);
+    },
+    tick: function () {
+        if (this.option.value === 1) {
+            this.option.disable = !this.option.disable;
+            clearInterval(this.interval);
+            this.option.text = this.option.textBak;
+            this.option.value = this.option.valueBak;
+            if (this.option.complete) this.option.complete();
+        } else {
+            this.option.value--;
+            this.option.text = this.option.waitingText.replace("{}", this.option.value);
+        }
+    }
+})
+
+new Button("#buttonContainer",
+    {
+        text: "普通按钮",
+        onClick: function (evt) {
+            alert(1);
+        }
+    });
+
+new SwitchButton("#switchButtonContainer",
+    {
+        text: "开关按钮",
+        open: function () {
+        },
+        close: function () {
+        }
+    });
+
+new CountdownButton("#countdownButtonContainer",
+     {
+         text: "获取短信验证码",
+         waitingText: "{}秒后可以重试",
+         value: 10,
+         complete: function () {
+             console.log(11);
+         }
+     });
